ef95ab74e1a57298f927faf9b6abbcaa8e767c9a
Ryu-ji-hyeon <dudvndwlgus@naver.com>
2024-09-20 21:40:29 +0900

    add : 사용자 응답 예외 처리

--- src/VoiceReservation/VoiceGuide.js
+++ src/VoiceReservation/VoiceGuide.js
@@ -105,7 +105,7 @@ const VoiceGuide = () => {
   const startVoiceGuide = () => {
     setError(null); // 에러 상태 초기화
     speak({
-      text: '안녕하세요. 음성 안내 예약 시스템을 사용하기 전에 몇 가지 주의사항을 알려드리겠습니다. 이 시스템은 음성 인식 기술을 사용하여 예약을 진행하니, 정확한 인식을 위해 조용한 환경에서 사용해 주세요. 또한, 이 대화는 품질 개선을 위해 녹음될 수 있습니다. 예약을 진행하시겠습니까?',
+      text: '안녕하세요. 음성 안내 예약 시스템을 사용하기 전에 몇 가지 주의사항을 알려드리겠습니다. 이 시스템은 음성 인식 기술을 사용하여 예약을 진행하니, 정확한 인식을 위해 조용한 환경에서 사용해 주세요. 또한, 이 대화는 품질 개선을 위해 녹음될 수 있습니다. 응답 시작 버튼을 누르고 응답 후 응답 종료 버튼을 누르시면 됩니다. 예약을 진행하시겠습니까?',
       lang: 'ko-KR',
     });
   };
@@ -133,28 +133,28 @@ const VoiceGuide = () => {
     setError(null); // 에러 초기화
   };
 
-  // 음성 인식 후 응답에 따른 이벤트 처리
-  const handleUserResponse = (response) => {
-    console.log('응답 처리 중...');
-    
-    // 여기서 인식된 텍스트(response)를 기반으로 다음 이벤트 처리
-    if (response.includes('네')) {
-      navigate('/Voice/DepartmentDoctorSelection'); // 예약 경로로 이동
-    } else if (response.includes('아니오')) {
-      navigate('/member/dashboard'); // 취소시 대시보드로 이동
-    } else {
-      setError('유효한 응답이 아닙니다. 다시 말해주세요.');
-      speak({
-        text: '유효한 응답이 아닙니다. 다시 말해주세요.',
-        lang: 'ko-KR',
-      });
-    }
-  };
+// 음성 인식 후 응답에 따른 이벤트 처리
+const handleUserResponse = (response) => {
+  console.log('응답 처리 중...');
+  
+  // 인식된 텍스트(response)를 기반으로 이벤트 처리
+  if (response.includes('네') || response.includes('응') || response.includes('어') || response.includes('녜') || response.includes('엉')) {
+    navigate('/Voice/DepartmentDoctorSelection'); // 예약 경로로 이동
+  } else if (response.includes('아니오') || response.includes('아니요') || response.includes('아니')) {
+    navigate('/member/dashboard'); // 취소시 대시보드로 이동
+  } else {
+    setError('유효한 응답이 아닙니다. 다시 응답해주세요.');
+    speak({
+      text: '유효한 응답이 아닙니다. 다시 응답해주세요.',
+      lang: 'ko-KR',
+    });
+  }
+};
 
   return (
     <ScreenContainer>
       <Content>
-        <Title>음성 안내</Title>
+        <Title>음성 예약 주의사항</Title>
         <Card>
           <Text>음성 안내 예약 시스템 주의사항</Text>
           <Button onClick={startVoiceGuide}>
